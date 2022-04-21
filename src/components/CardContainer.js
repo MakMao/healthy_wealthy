60c3a40cc4726c6d942df17345f89acdbaee01f5
Max <maxvanbeek@live.nl>
2022-04-21 11:00:03 +0200

    bugs fixed

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,4 +1,4 @@
-import React, {useState } from 'react'
+import React, {useState, useEffect } from 'react'
 import data from '../data/data'
 import styled from 'styled-components'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
